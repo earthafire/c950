9ef8b0fe2ef6742edc8ac8822193c4a3661a7b99
BuildTools <unconfigured@null.spigotmc.org>
2021-04-04 17:55:31 -0700

    added excel data importer and source files for wgups

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-# This is a sample Python script.
+# Addison Ashworth C950
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from exceldataimporter import *
 
 
 def print_hi(name):
@@ -9,8 +8,10 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
+# beginning of program
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('created by addison')
 
+    packages = get_packages_from_file("WGUPS Package File.csv")
+    #print(packages[0])
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ package.py
@@ -0,0 +1,11 @@
+class Package:
+    def __init__(self, packageid, address, city, state, zip, deadline, weight):
+        self.packageid = packageid
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.deadline = deadline
+        self.weight = weight
+
+
