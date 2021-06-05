1ced423e73fa93ae99d290f90de90fc5abb1ed8a
BuildTools <unconfigured@null.spigotmc.org>
2021-06-05 15:34:51 -0700

    package hash table rework

--- packagehashtable.py
+++ packagehashtable.py
@@ -1,36 +1,41 @@
-import package
-
-
-class HashItem:
-    def __init__(self, key, parcel):
-        self.key = key
-        self.package = parcel
-
-
 class PackageHashTable:
 
     # big O: O(1)
     def __init__(self):
         self.current_size = 25
         self.filled_slots = 0
-        self.data = [] * self.current_size  # list of packages
+        self.data = [None] * self.current_size  # list of packages
+
+    def get_hash(self, value):
+        return value % self.current_size
+
+    def is_full(self):  # check if array is close to full
+        if self.filled_slots > self.current_size - 10:
+            return True
+        else:
+            return False
+
+    def double_table_capacity(self):  # double size of current array
+        for x in range(0, self.current_size):
+            self.data.append(None)
+        self.current_size *= 2
 
     # big O: O(1)
     def add_package(self, new_package):
-        new_hash_item = HashItem(new_package.package_id, new_package)
-        packagenum = new_hash_item.key % self.current_size
-        self.data.insert(packagenum, new_hash_item)  # hash is current size
-        self.filled_slots += 1  # keeps track of how many full slots in order to prevent filling it up
+        package_num = self.get_hash(new_package.package_id)
+        print(package_num)
 
-        if self.filled_slots / self.current_size > .9:  # if the hashtable is getting full, add another 40 slots
-            # print("increasing size of hashtable")
-            self.current_size = self.current_size + 25
-            for x in range(0, 24):
-                self.data.append(None)
+        if self.data[package_num] is None:
+            self.filled_slots += 1  # keeps track of how many full slots in order to prevent filling it up
+
+        self.data[package_num] = new_package  # set index to new package
+
+        if self.is_full():  # if the hashtable is getting full, add another 40 slots
+            self.double_table_capacity()
 
     # big O(1) time to retrieve package
     def get_package(self, int_id):
-        return self.data[int_id]
+        return self.data[self.get_hash(int_id)]
 
     # big O(1) time to edit package
     def edit_package_status(self, int_key, status):
